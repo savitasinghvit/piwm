9729ce6f03267a352e931a5e5bdec4d830f293cc
anywhere-Dev01 <anywhere-dev01@anywhere-Dev01.(none)>
2013-01-21 12:50:41 +0530

    Minor changes

--- piwm.c
+++ piwm.c
@@ -8,33 +8,46 @@ void spawnXterm() {
         execvp("xterm", NULL);
     }
 }
+
 int main() {
     Display * display = XOpenDisplay(0x0);
+    char *piwm = "Piwm 0.1";
     Window root = DefaultRootWindow(display);
     XEvent event;
     int num_sizes;
     Rotation original_rotation;
     XWindowAttributes win_attr;
     XButtonEvent sev;
+    KeyCode t = XKeysymToKeycode(display, XStringToKeysym("t"));
+    KeyCode F = XKeysymToKeycode(display, XStringToKeysym("F"));
+    KeyCode F1 = XKeysymToKeycode(display, XStringToKeysym("F1"));
     XRRScreenSize *xrrs = XRRSizes(display, 0, &num_sizes);
     XRRScreenConfiguration *conf = XRRGetScreenInfo(display, root);
     short original_rate = XRRConfigCurrentRate(conf);
     SizeID original_size_id = XRRConfigCurrentConfiguration(conf, &original_rotation);
-    KeyCode t = XKeysymToKeycode(display, XStringToKeysym("t"));
-    KeyCode F = XKeysymToKeycode(display, XStringToKeysym("F"));
-    KeyCode F1 = XKeysymToKeycode(display, XStringToKeysym("F1"));
     if (!(display)) return 1;
     XGrabKey(display, AnyKey, ControlMask, root, True, GrabModeAsync, GrabModeAsync);
     XGrabButton(display, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
     XGrabButton(display, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+    char dark_grey[] = "#aaaaaa";
+    GC green_gc;
+    Colormap colormap;
+    XColor green_col;
+    colormap = DefaultColormap(display, 0);
+    green_gc = XCreateGC(display, RootWindow(display, DefaultScreen(display)), 0, 0);
+    XParseColor(display, colormap, dark_grey, &green_col);
+    XAllocColor(display, colormap, &green_col);
+    XSetForeground(display, green_gc, green_col.pixel);
     while (1) {
+        XDrawLine(display,RootWindow(display, DefaultScreen(display)), green_gc, 0, 20, xrrs[original_size_id].width-1, 20);
+        XDrawString(display, RootWindow(display, DefaultScreen(display)), green_gc, xrrs[original_size_id].width - 100, 15, piwm, strlen(piwm));
         XNextEvent(display, &event);
         if (event.type == KeyPress && event.xkey.subwindow == None) {
             if (event.xkey.keycode == t && event.xkey.state & ControlMask) spawnXterm();
         }
         if (event.type == KeyPress && event.xkey.subwindow != None) {
             if (event.xkey.keycode == F1 && event.xkey.state & ControlMask) XRaiseWindow(display, event.xkey.subwindow);
-            if (event.xkey.keycode == F && event.xkey.state & ControlMask) XMoveResizeWindow(display, event.xkey.subwindow, 0, 0, xrrs[original_size_id].width,  xrrs[original_size_id].height);
+            if (event.xkey.keycode == F && event.xkey.state & ControlMask) XMoveResizeWindow(display, event.xkey.subwindow, 0, 0, xrrs[original_size_id].width, xrrs[original_size_id].height);
         } else if (event.type == ButtonPress && event.xbutton.subwindow != None) {
             XGrabPointer(display, event.xbutton.subwindow, True, PointerMotionMask | ButtonReleaseMask, GrabModeAsync,
                     GrabModeAsync, None, None, CurrentTime);
